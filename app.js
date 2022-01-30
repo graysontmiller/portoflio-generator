ffe5025a3d2c7884c8eaf9b48c0bb641ac7d8a5d
Grayson Miller <graysonmiller@Graysons-MacBook-Pro.local>
2022-01-30 18:07:58 -0500

    added ability to add multiple projects

--- app.js
+++ app.js
@@ -55,14 +55,87 @@ const inquirer = require('inquirer')
     //? console.log('Portfolio complete! Checkout index.html to see the output!');
 //? });
 
-inquirer
-    .prompt ([
-        {
-            type:'input',
+const promptUser = () => {
+    return inquirer.prompt ([
+            {
+                type:'input',  // Input means the question will receive a text reply
+                name: 'name',
+                message: 'What is your name?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Enter your GitHub Username'
+            },
+            {
+                type: 'input',
+                name: 'about',
+                message: 'Provide some information about yourself:'
+            }
+        ]);
+    };
+        // The prompt section receives an array of objects in its argument, creating the individual question objects. What is typed in response is the answer.
+
+
+const promptProject = portfolioData => {
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+        return inquirer.prompt([
+          {
+            type: 'input',
             name: 'name',
-            message: 'What is your name?'
-        }
-    ])
-    // The prompt section receives an array of objects in its argument, creating the question object.
-    .then(answers => console.log(answers));
-    // The answer object is returned as a promise.
+            message: 'What is the name of your project?'
+          },
+          {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+          },
+          {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+          },
+          {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+          },
+          {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+          },
+          {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+          }
+        ])
+    .then(projectData => {
+            portfolioData.projects.push(projectData);
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData);
+            } else {
+                return portfolioData;
+            }
+        })
+        // After all of the questions, push this portfolio data to projects array. If the user answers true to the confirmAddProject, promptProject will be called.
+      };
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData);
+});
